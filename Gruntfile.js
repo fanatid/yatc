c023771514569d8ff6019ff65782492e3c234c53
Kirill Fomichev <fanatid@ya.ru>
2015-01-20 15:14:39 +0300

    gruntfile, jscs, jshint, travis

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      src: ['Gruntfile.js', 'src/*.js', 'test/*.js'],
+      options: {
+        jshintrc: true,
+        reporter: require('jshint-stylish')
+      }
+    },
+    jscs: {
+      src: ['Gruntfile.js', 'src/*.js', 'test/*.js'],
+      options: {
+        config: '.jscsrc'
+      }
+    },
+    mochaTest: {
+      test: {
+        src: ['test/*.js'],
+        options: {
+          reporter: 'spec'
+        }
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks('grunt-jscs')
+  grunt.loadNpmTasks('grunt-mocha-test')
+
+  grunt.registerTask('test', ['mochaTest'])
+  grunt.registerTask('default', ['jshint', 'jscs', 'test'])
+}
