ac9dc036e0b628e844c569d60f89ecf1529cb21c
Kirill Fomichev <fanatid@ya.ru>
2015-01-22 20:49:43 +0300

    update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,26 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      production: {
+        src: ['src/index.js'],
+        dest: 'build/yatc.js',
+        options: {
+          browserifyOptions: {
+            standalone: 'yatc'
+          }
+        }
+      },
+      test: {
+        src: ['test/*.js', '!test/yatc.js'],
+        dest: 'test/yatc.js'
+      }
+    },
+    clean: {
+      builds: {
+        src: ['build', 'test/yatc.js']
+      }
+    },
     jshint: {
       src: ['Gruntfile.js', 'src/*.js', 'test/*.js'],
       options: {
@@ -39,6 +59,13 @@ module.exports = function (grunt) {
           reporter: 'spec'
         }
       }
+    },
+    uglify: {
+      production: {
+        files: {
+          'build/yatc.min.js': 'build/yatc.js'
+        }
+      }
     }
   })
 
@@ -52,13 +79,18 @@ module.exports = function (grunt) {
     })
   })
 
+  grunt.loadNpmTasks('grunt-browserify')
+  grunt.loadNpmTasks('grunt-contrib-clean')
+  grunt.loadNpmTasks('grunt-contrib-connect')
   grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
   grunt.loadNpmTasks('grunt-jscs')
   grunt.loadNpmTasks('grunt-mocha-istanbul')
+  grunt.loadNpmTasks('grunt-mocha-phantomjs')
   grunt.loadNpmTasks('grunt-mocha-test')
 
+  grunt.registerTask('build', ['browserify:production', 'uglify:production'])
   grunt.registerTask('coverage', ['mocha_istanbul:coverage'])
   grunt.registerTask('coveralls', ['mocha_istanbul:coveralls'])
   grunt.registerTask('test', ['mochaTest'])
-  grunt.registerTask('default', ['jshint', 'jscs', 'test'])
 }
